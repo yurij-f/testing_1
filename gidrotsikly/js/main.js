518945ce990bb6b7f78a472016d23add13b0d7e6
Yuriy Fedosov <fedosovyurij2017@gmail.com>
2020-09-25 15:29:17 +0300

    version_1.0

---
+++ gidrotsikly/js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+	$('.banner-section__slider').slick({
+	dots: true,
+	prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
+	nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>'
+	});
+
+	$('.tab').on('click', function(e){
+		
+		e.preventDefault();
+
+		$('.tab').removeClass('tab--active');
+		$('.tabs-content').removeClass('tabs-content--active');
+		
+		$(this).addClass('tab--active');
+
+		$($(this).attr('href')).addClass('tabs-content--active');
+
+	});
+
+	$('.product-item__favorite').on('click', function(){
+	$('.product-item__favorite').toggleClass('product-item__favorite--active')
+	})
+
+	});
