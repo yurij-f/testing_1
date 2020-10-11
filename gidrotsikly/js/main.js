a4549eb5637849b7245a38447e78e66d8a6ac0a6
Yuriy Fedosov <fedosovyurij2017@gmail.com>
2020-10-11 17:02:57 +0300

    version_3.2

--- gidrotsikly/js/main.js
+++ gidrotsikly/js/main.js
@@ -55,4 +55,10 @@ $(function(){
 		$('.product-item__wrapper').addClass('product-item__wrapper--list');
 	});
 
+	$(".rate-yo").rateYo({
+		ratedFill: "#1C62CD",
+		spacing: "7px",
+		normalFill: "#C4C4C4"
+	});
+
 	});
