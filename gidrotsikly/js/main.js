156d874dfb7740d0ecd2e11ebea009ec3099ad86
Yuriy Fedosov <fedosovyurij2017@gmail.com>
2020-10-17 17:28:33 +0300

    version_3.4

--- gidrotsikly/js/main.js
+++ gidrotsikly/js/main.js
@@ -2,7 +2,15 @@ $(function(){
 	$('.banner-section__slider').slick({
 	dots: true,
 	prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
-	nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>'
+	nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>',
+		responsive: [
+			{
+				breakpoint: 969,
+				settings: {
+					arrows: false
+				}
+			}
+		]
 	});
 
 	$('.tab').on('click', function(e){
@@ -27,7 +35,39 @@ $(function(){
 	slidesToScroll: 1,
 	prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt=""></button>',
 	nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt=""></button>',
-
+	responsive: [
+			{
+				breakpoint: 1301,
+				settings: {
+					arrows: false,
+					dots: true
+				}
+			},
+		{
+				breakpoint: 1201,
+				settings: {
+					slidesToShow: 3,
+					arrows: false,
+					dots: true
+				}
+		},
+		{
+				breakpoint: 870,
+				settings: {
+					slidesToShow: 2,
+					arrows: false,
+					dots: true
+				}
+		},
+		{
+				breakpoint: 590,
+				settings: {
+					slidesToShow: 1,
+					arrows: false,
+					dots: true
+				}
+		}
+	]
 	});
 
 	$('.filter-style').styler();
